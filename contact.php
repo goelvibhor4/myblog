607a4107fec9edd2764561353998d2a03d12eac8
vibhor <goelvibhor4@gmail.com>
2015-07-03 17:02:27 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,4 +1,12 @@
 <?php
-
-
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Phone: $phone \n message: $message \n";
+$recipient = "goelvibhor4@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='goelvibhor.net' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
