6565195da8a12783f1c7d1ff1be72f6973e7922c
vibhor <goelvibhor4@gmail.com>
2015-07-25 16:20:46 +0530

    Update send.php

--- send.php
+++ send.php
@@ -1,26 +1,11 @@
 <?php
-
-// Here we get all the information from the fields sent over by the form.
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-
-	$to = 'goelvibhor4@gmail.com';
-	$subject = 'the subject';
-	$message = 'FROM: '.$name.' Email: '.$email.'Message: '.$message;
-	$headers = 'From: akshay.mittal0894@gmail.com' . "\r\n";
-
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // this line checks that we have a valid email address
-    mail($to, $subject, $message, $headers); //This method sends the mail.
-	echo "Your email was sent!"; // success message
-}else{
-	echo "Invalid Email, please provide an correct email.";
-}
-	
-
-	
-	
-	
-
-
+$formcontent=" From: $name \n message: $message \n";
+$recipient = "goelvibhor4@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='http://focalpoint.ieeemsit.in' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
